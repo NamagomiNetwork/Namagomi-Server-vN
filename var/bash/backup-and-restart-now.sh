0d31e7d2713cf28ac9ac8e7457f5d2b65496036e
ikazuchizero <ichiouningendayo@gmail.com>
2023-10-11 04:03:35 +0900

    きっくするようにした

--- var/bash/backup-and-restart-now.sh
+++ var/bash/backup-and-restart-now.sh
@@ -2,6 +2,8 @@
 
 sudo bash ./alertjs.sh
 
+sudo -u minecraft screen -S mc-server -X stuff "kick @a 再起動後に再接続してください。byごみ\015"
+
 sudo systemctl stop mc-server
 
 sudo bash ./backup.sh
--- var/bash/backup-and-restart.sh
+++ var/bash/backup-and-restart.sh
@@ -2,6 +2,8 @@
 
 sudo bash ./alert.sh
 
+sudo -u minecraft screen -S mc-server -X stuff "kick @a 再起動後に再接続してください。byごみ\015"
+
 sudo systemctl stop mc-server
 
 sudo bash ./backup.sh
