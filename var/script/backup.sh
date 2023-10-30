09a84e5e0f8fdc8c02338b1ee6ffa0e9fa251d0a
ikazuchizero <ichiouningendayo@gmail.com>
2023-10-31 07:17:36 +0900

    a

--- var/script/backup.sh
+++ var/script/backup.sh
@@ -5,7 +5,7 @@ MC_PATH='/var/mc-server'
 # バックアップ収納パス
 BK_PATH='/var/mc-backups'
 # バックアップ時刻形式
-BK_TIME=`date +%Y-%m-%d-%H:%M:%S`
+BK_TIME=`date +%Y'-'%m'-'%d'-'%H':'%M':'%S`
 # バックアップファイル名
 BK_NAME="${BK_PATH}/mc_backup_${BK_TIME}.tar.gz"
 # バックアップ保存世代数
